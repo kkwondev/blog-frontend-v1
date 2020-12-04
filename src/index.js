e7a8492753c07a528b3318dfc1cb2b943a891653
kkwoncokr <kkwoncokr@gmail.com>
2020-12-04 18:30:53 +0900

    ADD : 로그인 회원가입 페이지 퍼블리싱

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
 
+const store = createStore(rootReducer, composeWithDevTools())
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
