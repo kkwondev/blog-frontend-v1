5275ab85f9da9e68adedf52863a5bc5b6177d295
DESKTOP-SH1ABKH\user <kkwoncokr@gmail.com>
2020-12-13 21:48:00 +0900

    UPDATE : 쿠키공유.

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: 'http://54.180.186.118'
+    baseURL: 'http://54.180.186.118',
+    withCredentials:true,
 });
 
 export default client;
